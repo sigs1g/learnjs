26979415c2fe1cf681f96ce3a5c5f47c14f52597
MorishigeShogo <s1g@s1g-mb.local>
2017-08-06 13:08:21 +0900

    add skip-btn

--- public/app.js
+++ public/app.js
@@ -20,6 +20,10 @@ learnjs.appOnReady = function() {
   learnjs.showView(window.location.hash);
 }
 
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name,args);
+}
+
 learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
@@ -56,6 +60,15 @@ learnjs.problemView = function(data) {
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(problemData, view);
+
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
   return view;
 }
 
@@ -68,6 +81,7 @@ learnjs.showView = function(hash) {
   var hashParts = hash.split('-');;
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
